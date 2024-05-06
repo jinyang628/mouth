647c621109a8d076f53d23693e9a784efa7f8b29
jinyang628 <chenjinyang4192@gmail.com>
2024-05-05 17:23:22 -0700

    Update

--- app/scripts/api/entry/_post.ts
+++ app/scripts/api/entry/_post.ts
@@ -4,15 +4,14 @@ import axios from 'axios';
 export async function post(shareGptLinks: string[], STOMACH_API_URL: string, API_KEY: string) {
     try {
         console.log("Making POST request with links:", shareGptLinks)
-        // TEST WITH FIRST LINK
+        // TEST WITH FIRST LINK TODO: TALK TO SHAWN AND DECIDE HOW MANY LINKS WE PASS IN/HOW TO REDIRECT USER
         const link: string = shareGptLinks[0];
-        console.log(`${STOMACH_API_URL}/api/entry`)
         const response = await axios.post(
             `${STOMACH_API_URL}/api/entry`, 
             {
                 api_key: API_KEY,
                 url: link,
-                tasks: ["summary"]
+                tasks: ["summarise"]
             }
         );
         console.error(response)
